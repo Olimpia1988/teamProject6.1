88c7665b674f45a2c2ab2b7becf6cf306b91c76d
Olimpia <olimpiacompean@prusuit.org>
2019-09-13 15:21:46 -0400

    add git ignore file

---
+++ teamProject6.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  teamProject6.1
+//
+//  Created by Olimpia on 9/13/19.
+//  Copyright © 2019 Olimpia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
